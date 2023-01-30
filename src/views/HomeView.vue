7c580e7b0129f843814725648e8ae1332825d92d
Jose Gonzalez <joseignacio4040@gmail.com>
2023-01-30 02:00:49 -0400

    Find song by artist first version

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,116 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="main-content" class="d-flex flex-column align-center justify-center">
+    <h1 class="mb-7">Buscador de musica</h1>
+    <div class="d-flex align-center justify-center find-input mb-6">
+      <v-text-field
+        class="mr-5"
+        label="Artisa o Banda Musical"
+        placeholder="Ingresa el nombre"
+        filled
+        background-color="white"
+        hide-details=true
+        v-model="search"
+      ></v-text-field>
+      <v-btn @click="getSongs">
+        Buscar
+        <v-icon >mdi-magnify</v-icon>
+      </v-btn>
+    </div>
+    <v-progress-circular
+      v-if="loading"
+      indeterminate
+      color="white"
+    ></v-progress-circular>
+    <div v-else-if="!songs?.results || songs?.results < 1" class="white--text">
+      No hay resultados para mostrar
+    </div>
+    <v-simple-table id="songsTable" class="mb-10 elevation-10" v-else >
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">
+              Nombre cancíon
+            </th>
+            <th class="text-left">
+              Nombre Almbum
+            </th>
+            <th class="text-left">
+              Url
+            </th>
+            <th class="text-left">
+              Precio
+            </th>
+            <th class="text-left">
+              Fecha de lanzamiento
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(item, index) in songs.results"
+            :key="index"
+          >
+            <td>{{ item.trackName }}</td>
+            <td>{{ item.collectionName }}</td>
+            <td> <a :href="item.previewUrl"> Preview </a> </td>
+            <td>{{ item.trackPrice }}</td>
+            <td>{{ item.releaseDate | moment("DD-MM-YYYY") }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  name: "Home-component",
+  components: {},
+  mounted() {
+    this.getSongs()
+  },
+  data: () => ({
+    songs: '',
+    search: 'Shakira',
+    loading: true
+  }),
+  methods: {
+    getSongs:  function () {
+      this.loading = true
+      const params = {
+        // artistName: 'Juan Gabriel'
+        artistName: this.search
+      }
+
+      axios.get(
+        'http://localhost:3000/songs',
+        {
+          params
+        }
+      )
+      .then((response) => {
+        console.log(response.data, 'songs')
+        this.songs = response.data
+        this.loading = false
+      })
+    }
   }
-}
+};
 </script>
+
+<style scoped>
+#main-content {
+  margin-top: 90px;
+}
+#songsTable {
+  max-width: 80%;
+}
+.find-input {
+  width: 45%;
+}
+.c-shadow-1{
+  box-shadow: 5px 5px;
+}
+</style>
